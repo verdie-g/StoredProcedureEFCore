fa5671a4540db4941013aede18c22c02a1aee93e
Grégoire Verdier <gregoire.verdier@3ie.fr>
2017-03-29 22:01:53 +0200

    Clear cache & recompile stored procedure before using it

--- TestDB/dbo/Stored Procedures/ListAll.sql
+++ TestDB/dbo/Stored Procedures/ListAll.sql
@@ -3,13 +3,17 @@
 -- Create date: 29/03/2017
 -- Description:	
 -- =============================================
-CREATE PROCEDURE ListAll
+CREATE PROCEDURE [dbo].[ListAll]
 	@p bigint = 0
+WITH RECOMPILE  
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
+
+	--CHECKPOINT
+	--DBCC DROPCLEANBUFFERS
 
     -- Insert statements for procedure here
 	SELECT * FROM Table_1;
