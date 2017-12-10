aa5c274ad3700bbfd7b4fd17d4698cb7486d8dee
Grégoire <gregoire.verdier@gmail.com>
2017-12-10 15:06:22 +0100

    More concise variable names

--- StoredProcedure/DbTools.cs
+++ StoredProcedure/DbTools.cs
@@ -13,13 +13,13 @@ namespace StoredProcedure
     /// Call a stored procedure
     /// </summary>
     /// <typeparam name="T">Type of the result object</typeparam>
-    /// <param name="context"></param>
+    /// <param name="ctx"></param>
     /// <param name="name">Procedure's name</param>
     /// <param name="parameters">Procedure's parameters</param>
     /// <returns></returns>
-    public static List<T> Exec<T>(this DbContext context, string name, params (string, object)[] parameters)
+    public static List<T> Exec<T>(this DbContext ctx, string name, params (string, object)[] parameters)
     {
-      using (IDataReader reader = CreateDbCommand(context, name, parameters).ExecuteReader())
+      using (IDataReader reader = CreateDbCommand(ctx, name, parameters).ExecuteReader())
       {
         return reader.ToList<T>();
       }
@@ -28,36 +28,36 @@ namespace StoredProcedure
     /// <summary>
     /// Call a stored procedure that only return a boolean
     /// </summary>
-    /// <param name="context"></param>
+    /// <param name="ctx"></param>
     /// <param name="name">Procedure's name</param>
     /// <param name="parameters">Procedure's parameters</param>
     /// <returns></returns>
-    public static bool Exec(this DbContext context, string name, params (string, object)[] parameters)
+    public static bool Exec(this DbContext ctx, string name, params (string, object)[] parameters)
     {
-      DbCommand command = CreateDbCommand(context, name, parameters);
-      DbParameter returnParameter = command.CreateParameter();
+      DbCommand cmd = CreateDbCommand(ctx, name, parameters);
+      DbParameter returnParameter = cmd.CreateParameter();
       returnParameter.ParameterName = "@out";
       returnParameter.DbType = DbType.Boolean;
       returnParameter.Direction = ParameterDirection.ReturnValue;
-      command.Parameters.Add(returnParameter);
-      command.ExecuteNonQuery();
+      cmd.Parameters.Add(returnParameter);
+      cmd.ExecuteNonQuery();
       return Convert.ToBoolean(returnParameter.Value);
     }
 
-    private static DbCommand CreateDbCommand(DbContext context, string name, params (string name, object value)[] parameters)
+    private static DbCommand CreateDbCommand(DbContext ctx, string name, params (string name, object value)[] parameters)
     {
-      DbCommand command = context.Database.GetDbConnection().CreateCommand();
-      command.CommandType = CommandType.StoredProcedure;
-      command.CommandText = name;
+      DbCommand cmd = ctx.Database.GetDbConnection().CreateCommand();
+      cmd.CommandType = CommandType.StoredProcedure;
+      cmd.CommandText = name;
       foreach (var parameter in parameters)
       {
-        DbParameter param = command.CreateParameter();
+        DbParameter param = cmd.CreateParameter();
         param.ParameterName = '@' + parameter.name;
         param.Value = parameter.value;
-        command.Parameters.Add(param);
+        cmd.Parameters.Add(param);
       }
-      context.Database.OpenConnection();
-      return command;
+      ctx.Database.OpenConnection();
+      return cmd;
     }
 
     /// <summary>
