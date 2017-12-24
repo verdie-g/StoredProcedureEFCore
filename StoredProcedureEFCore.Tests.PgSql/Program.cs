4a096634552e5a5d5b7e5f9f6019f94ba6818d16
Grégoire <gregoire.verdier@gmail.com>
2017-12-24 14:29:13 +0100

    Add pgSQL test project

---
+++ StoredProcedureEFCore.Tests.PgSql/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace StoredProcedureEFCore.Tests.PgSql
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      DbContext ctx = new TestContext();
+
+      List<Model> rows = null;
+
+      ctx.LoadStoredProc("public.listall")
+         .AddParam("llimit", 300)
+         // .AddOutputParam("limitOut", out IOutputParam<long> limitOut)
+         .Exec(r => rows = r.ToList<Model>());
+
+      ctx.LoadStoredProc("public.ReturnBoolean")
+         .AddParam("boolean_to_return", true)
+         .ReturnValue(out IOutParam<bool> retParam)
+         .ExecNonQuery();
+
+      bool b = retParam.Value;
+
+      ctx.LoadStoredProc("public.ListAll")
+         .AddParam("limit", 1)
+         .ExecScalar(out long l);
+
+      Console.WriteLine(l);
+    }
+  }
+}
---
+++ StoredProcedureEFCore.Tests.PgSql/TestContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StoredProcedureEFCore.Tests.PgSql
+{
+  public partial class TestContext : DbContext
+  {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        optionsBuilder.UseNpgsql("Host=localhost;Database=test;Username=postgres;Password=root");
+      }
+    }
+  }
+}
