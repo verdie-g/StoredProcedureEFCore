b224e71a8245b82ca93183a806eebe42bfe2272e
Grégoire <gregoire.verdier@gmail.com>
2018-01-31 16:47:56 +0100

    Add null values to sql server tests

--- StoredProcedureEFCore.Tests.SqlServer.Database/dbo/Tables/Table_1.sql
+++ StoredProcedureEFCore.Tests.SqlServer.Database/dbo/Tables/Table_1.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[Table_1] (
     [id]                   BIGINT        IDENTITY (1, 1) NOT NULL,
-    [name]                 VARCHAR (255) NOT NULL,
+    [name]                 VARCHAR (255) NULL,
     [date]                 DATETIME      NOT NULL,
     [active]               BIT           NOT NULL,
     [name_with_underscore] INT           CONSTRAINT [DF_Table_1_name_with_underscore] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_Table_1] PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+
 
