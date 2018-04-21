a538c6916e2d94fc5133147d951ac28a1a698917
Grégoire <gregoire.verdier@gmail.com>
2018-04-21 11:30:50 +0200

    Add instrumentation for fixed size output param #12

---
+++ StoredProcedureEFCore.Tests.SqlServer.Database/dbo/Stored Procedures/OutputFixedSize.sql
@@ -0,0 +1,13 @@
+﻿-- =============================================
+-- Author:		Grégoire Verdier
+-- Create date: 21/04/2018
+-- Description:
+-- =============================================
+CREATE PROCEDURE [dbo].[OutputFixedSize]
+  @fixed_size varchar OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+  SET @fixed_size = 'Jambon Beurre'
+END
--- StoredProcedureEFCore.Tests.SqlServer/Program.cs
+++ StoredProcedureEFCore.Tests.SqlServer/Program.cs
@@ -54,6 +54,12 @@ namespace StoredProcedureEFCore.Tests.SqlServer
       await ctx.LoadStoredProc("dbo.SelectParam")
         .AddParam<int?>("n", null)
         .ExecScalarAsync<int?>(i => Console.WriteLine(i));
+
+      await ctx.LoadStoredProc("dbo.OutputFixedSize")
+        .AddParam("fixed_size", out IOutParam<string> fixedSizeParam)
+        .ExecNonQueryAsync();
+
+      string s = fixedSizeParam.Value;
     }
   }
 }
