b09f0724c71fcc70bc13936330fc6b97987abbc7
Michael Kühberger <kuehberger@ebps.at>
2019-09-16 17:06:43 +0200

    Allow null-Values for OUTPUT parameter

--- StoredProcedureEFCore/OutputParameter.cs
+++ StoredProcedureEFCore/OutputParameter.cs
@@ -3,17 +3,31 @@ using System.Data.Common;
 
 namespace StoredProcedureEFCore
 {
-  internal class OutputParam<T> : IOutParam<T>
-  {
-    public OutputParam(DbParameter param)
+    internal class OutputParam<T> : IOutParam<T>
     {
-      _dbParam = param;
+        public OutputParam(DbParameter param)
+        {
+            _dbParam = param;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (_dbParam.Value is System.DBNull)
+                {
+                    if (Nullable.GetUnderlyingType(typeof(T)) != null)
+                    {
+                        return default(T);
+                    }
+                }
+
+                return (T)Convert.ChangeType(_dbParam.Value, typeof(T));
+            }
+        }
+
+        public override string ToString() => _dbParam.Value.ToString();
+
+        private DbParameter _dbParam;
     }
-
-    public T Value => (T)Convert.ChangeType(_dbParam.Value, typeof(T));
-
-    public override string ToString() => _dbParam.Value.ToString();
-
-    private DbParameter _dbParam;
-  }
 }
